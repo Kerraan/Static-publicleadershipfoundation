88c5fc4b9c6edf9991bfd95e43b2bee6a06c3bbe
Dirk <99747924+Kerraan@users.noreply.github.com>
2022-11-04 14:41:05 +0100

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPassthroughCopy("./src/admin/");
 
   eleventyConfig.addFilter("eventDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(
