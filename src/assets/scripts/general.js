a9c8fc28911dd1a14e2307449b0e51954a4adf72
Dirk <99747924+Kerraan@users.noreply.github.com>
2022-11-04 14:21:14 +0100

    added netlify cms v2

--- src/assets/scripts/general.js
+++ src/assets/scripts/general.js
@@ -3,13 +3,3 @@
 console.log("Hello World");
 
 // Netlify CMS
-
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", (user) => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
